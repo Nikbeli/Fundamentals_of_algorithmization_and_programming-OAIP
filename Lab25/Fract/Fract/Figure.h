02c9a384a7a68a202e6f6bf813dc1d5776d2406b
nikbel2004@outlook.com <nikbel2004@outlook.com>
2023-12-01 02:26:06 +0400

    Лабораторная работа 26

--- Lab25/Fract/Fract/Figure.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Windows.h"
-
-
-void IndirectRecursiveCircle(HDC hdc, int cx, int cy, int size);
-void IndirectRecursiveTriangle(HDC hdc, int cx, int cy, int size);
--- Lab25/LabFractal/LabFractal/Figure.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Windows.h"
-
-void RecursiveImage1_1(HDC hdc, int cx, int cy, int size);
-void RecursiveImage1_2(HDC hdc, int cx, int cy, int size);
-void RecursiveImage1_3(HDC hdc, int cx, int cy, int size);
-void RecursiveImage1_4(HDC hdc, int cx, int cy, int size);
-void RecursiveImage1_5(HDC hdc, int cx, int cy, int size);
